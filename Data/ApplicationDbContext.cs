1faf605033ae85cc89f8260cb23045bb86894ec5
Felipe-Tabares <aftb20053@gmail.com>
2025-11-10 21:12:17 -0500

    Initial commit: ASP.NET Core Razor Pages app with EF Core, Identity, and CRUD operations

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyWebApp.Models;
+
+namespace MyWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
